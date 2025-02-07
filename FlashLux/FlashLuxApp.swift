fd994b1457df0e97b0c9d37ab308ed48f232b1f1
sophiekopaliani <sophie.kopaliani@gmail.com>
2025-02-08 00:23:02 +0400

    Initial Commit

---
+++ FlashLux/FlashLuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashLuxApp.swift
+//  FlashLux
+//
+//  Created by Sophio Kopaliani on 08.02.25.
+//
+
+import SwiftUI
+
+@main
+struct FlashLuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashLuxTests/FlashLuxTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlashLuxTests.swift
+//  FlashLuxTests
+//
+//  Created by Sophio Kopaliani on 08.02.25.
+//
+
+import Testing
+
+struct FlashLuxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
